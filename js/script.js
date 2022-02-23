0885335717942789c4cce16b61cb630706c01a66
rowl <airowl.gasga@gmail.com>
2022-02-23 16:37:23 +0100

    inserito il button con event listener del click

--- js/script.js
+++ js/script.js
@@ -28,34 +28,45 @@ const user = document.getElementById('my-user');
 const kmUser = document.getElementById('my-km-user');
 const ageUser = document.getElementById('my-age-user');
 
-const under18 = ageUser.value;
 
-console.log(user.value);
-console.log(kmUser.value);
-console.log(under18);
-
-// calcolo del prezzo
-const price = 0.27 * kmUser;
-
-console.log(price);
-
-// calcolo dello sconto per età
-if (ageUser < 18) {
-
-    console.log('è minorenne');
-
-    const disUnder18 = (17 / 100);
-
-    const output = price - disUnder18;
-
-} else if (ageUser >= 65) {
-    console.log('ha 65 anni in su');
-    const disUnder18 = (33 / 100);
+// console.log(user.value);
+// console.log(parseFloat(kmUser.value));
+// console.log(ageUser.value);
 
 
-} else {
-    console.log('tra i 19 e 64 anni');
-}
+// button per l'evento
 
-// Prezzo assoluto
-console.log(output);
+const button = document.querySelector('#my-submit');
+
+button.addEventListener('click', function() {
+    // calcolo del prezzo
+    const kmPrice = 0.27;
+    let price = kmPrice * parseFloat(kmUser.value);
+
+    console.log(price);
+
+    // calcolo dello sconto per età
+    if (ageUser.value == 'under18') {
+
+        console.log('è minorenne');
+
+        const disUnder18 = (17 / 100);
+
+        price *= disUnder18;
+
+    } else if (ageUser.value == 'over65') {
+
+        console.log('ha 65 anni in su');
+
+        const divOver65 = (33 / 100);
+
+        price *= disOver65;
+
+    } else {
+        console.log('tra i 19 e 64 anni');
+    }
+
+    // Prezzo assoluto
+    console.log(price);
+});
+
