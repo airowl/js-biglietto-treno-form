6f32be0ec5fec335b835c0ba2177fc0cfd2a39d5
rowl <airowl.gasga@gmail.com>
2022-02-23 15:05:25 +0100

    descritto i passaggi da fare su js

---
+++ js/script.js
@@ -0,0 +1,22 @@
+/* 
+    chiedere all'utente i dati che di servono
+    -   Nome e cognome del passegero
+    -   Numero di km percorsi
+    -   Età del passeggero con il select (Maggiorenne o Minorenne)
+
+    Calcolo del prezzo
+
+    Calcolo dello sconto in base
+    -   se under 18 avrà uno sconto del 17%
+    -   se over 65 avrà uno sconto del 33%
+
+    Presentare il prezzo ultimato
+
+    Calcolo randomico della carozza
+
+    Calcolo randomico del codice CP // facoltativa
+
+    Presentare il tutto sull'interfaccia utente
+
+
+*/ 
